20e89a2a2734c3ff534f26a8fc101ebd48b7a105
andreaa21 <andrea.zollini@proton.me>
2022-10-25 15:14:21 +0200

    Milestone 0

--- js/script.js
+++ js/script.js
@@ -20,3 +20,46 @@ Stampare delle card formattate contenete immagini e testo (La grafica fornita è
 **BONUS:**
 Aggiungere degli aggettivi (più di uno) che identifichino ogni persona e stamparli nella card
 */
+
+
+// MILESTONE 0
+
+const team =[
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        name: 'Walter Gordon',
+        role: 'Founder & CEO',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer'
+    }
+]
+
+
